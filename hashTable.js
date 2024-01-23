ee350204ce83de4ba00ff45faecdaa82714c6517
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-01-23 18:03:17 -0500

    hastable get method solved

--- hashTable.js
+++ hashTable.js
@@ -21,23 +21,30 @@
      if(typeof key!=="string")throw TypeError("value must be string")
      if(!this.array[index]) this.array[index]=[]
         
-     if (!this.array[index].hasOwnProperty(key)) {
+     if (!this.array[index]) {
         // Si la clave no existe, crear un array para la clave
         this.array[index][key] = [];
     }
 
     // Agregar el nuevo valor al array asociado con la clave
-    this.array[index][key].push([key,value]);
+    this.array[index].push([key,value]);
     
     }
+    Hashtable.prototype.get=function(key){
+      let index = this.hash(key)
+      let valuesArr= this.array[index].map((arr)=>{if(arr[0]===key){return arr}})
+      console.log(valuesArr)
+      return valuesArr
+     }
     let myTable = new Hashtable();
 
  myTable.set("juliana",54)
- myTable.set("juliana",5)
- myTable.set("juliana",544)
+ myTable.set("juan",5)
+ myTable.set("julia",544)
  myTable.set("juliana",542)
  myTable.set("juliana",54)
- myTable.set("juliana",52)
+ myTable.set("auan",52)
  myTable.set("juliana",542)
+ myTable.get("juan")
  
  console.log(myTable)

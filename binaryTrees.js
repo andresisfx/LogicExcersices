13be4697fb655ee586cef29bbf804e2c8aefb887
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-01-25 13:35:30 -0500

    size binary tree method solved

--- binaryTrees.js
+++ binaryTrees.js
@@ -18,19 +18,19 @@ function BinarySearchTree(value) {
           }
        
     }
- 
+   }
     BinarySearchTree.prototype.depthFirstForEach=function(cb,order){
        //in-order left-root-rigth
        //pre-order root-left-rigth
        //post-order left-rigth-root
        if(order==="in-order"){
           if(this.left&&this.left.depthFirstForEach(cb,order));
-          cb(this.root)
+          cb(this.root);
           if(this.right&&this.left.depthFirstForEach(cb,order));
  
        }
        if(order==="pre-order"){
-            cb(this.root)
+            cb(this.root);
             if(this.left&&this.left.depthFirstForEach(cb,order));
             if(this.right&&this.left.depthFirstForEach(cb,order));
 
@@ -38,11 +38,19 @@ function BinarySearchTree(value) {
        if(order==="post-order"){
             if(this.left&&this.left.depthFirstForEach(cb,order));
             if(this.right&&this.left.depthFirstForEach(cb,order));
-            cb(this.root)
+            cb(this.root);
 
      }
     }
- }
+    BinarySearchTree.prototype.size=function(){
+      if(this.left===null&&this.right===null)return 1;
+      if(this.left!==null&&this.right===null)return 1 +this.left.size();
+      if(this.left===null&&this.right!==null)return 1+this.right.size();
+      if(this.left!==null&&this.right!==null)return 1+this.right.size()+ this.left.size();
+
+      
+   }
+ 
  let myTree = new BinarySearchTree(10)
  myTree.insert(8)
  myTree.insert(11)

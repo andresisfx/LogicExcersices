006187a1ff19622f0bbf9dcf2b859abdef1a2a9d
andresisfx <124216395+andresisfx@users.noreply.github.com>
2024-05-17 19:37:21 -0500

    findIndex excersise solved

--- commonQuestions.js
+++ commonQuestions.js
@@ -15,3 +15,24 @@ function isAnagram (s,t){
   return true
 }
 console.log(isAnagram("anagrama","GRAMAANA"))
+
+
+
+// sum two numbers 
+// encontrar los indices de los dos numero que sumados nos da target
+
+
+function findIndex(arr,target){
+    let seen=[]
+    for (let i = 0; i < arr.length; i++) {
+        let complement=(target-arr[i])
+        if(seen.includes(complement)){
+            return  `${arr.indexOf(complement)}, ${arr.indexOf(arr[i])}`
+        }else{
+            seen.push(arr[i])
+        }
+
+    }
+}
+
+console.log(findIndex([1,6,2,3,7],9))

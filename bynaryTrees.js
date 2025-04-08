e8b0094d4007ddf188ee750da55d9bc94718b682
andresisfx <andresrodriguezwg@gmail.com>
2025-04-08 16:59:05 -0500

    new modificationinbinary serach tree

--- bynaryTrees.js
+++ bynaryTrees.js
@@ -32,60 +32,113 @@
 
 
 
+// class Nodo {
+//     constructor(valor){
+//       this.valor=valor;
+//       this.izquerdo=null;
+//       this.derecho=null;
+//     }
+// }
+
+// class ArbolBinario {
+//      constructor (){
+        
+//         this.raiz=null;
+//      }
+
+//      insertarValorEnArbol=(valor)=>{
+//         const nuevoNodo =new Nodo(valor)
+//         if(!this.raiz){
+//             this.raiz=nuevoNodo
+//         }else{
+//             this.insertarNodo(this.raiz,nuevoNodo)
+//         }
+//      }
+
+//      insertarNodo=(nodo, nuevonodo)=>{
+//         if(nuevonodo.valor===nodo.valor){
+//             console.log("Este valor ya existe")
+//          return 
+//         }   
+//         if(nuevonodo.valor<nodo.valor){
+//             if(!nodo.izquerdo){
+//                 nodo.izquerdo=nuevonodo
+//             }else{
+//                 this.insertarNodo(nodo.izquerdo,nuevonodo)
+//             }
+//         }else{
+//             if(!nodo.derecho){
+//                 nodo.derecho=nuevonodo;
+//             }else{
+//                 this.insertarNodo(nodo.derecho,nuevonodo)
+//             }
+//         }
+        
+//      }
+// }
+     
+
+
+// const miArbol=new ArbolBinario()
+// miArbol.insertarValorEnArbol(15)
+// miArbol.insertarValorEnArbol(16)
+// miArbol.insertarValorEnArbol(17)
+// miArbol.insertarValorEnArbol(18)
+// miArbol.insertarValorEnArbol(14)
+// miArbol.insertarValorEnArbol(13)
+// miArbol.insertarValorEnArbol(11)
+// miArbol.insertarValorEnArbol(12)
+// console.log(JSON.stringify(miArbol, null, 2));
+
+
+
 class Nodo {
-    constructor(valor){
-      this.valor=valor;
-      this.izquerdo=null;
-      this.derecho=null;
+    constructor(value){
+        this.value=value
+        this.izquierdo=null;
+        this.derecho=null
     }
 }
 
 class ArbolBinario {
-     constructor (){
-        
-        this.raiz=null;
-     }
+  constructor(){
+    this.raiz=null
+  }
 
-     insertarValorenArbol=(valor)=>{
-        const nuevoNodo =new Nodo(valor)
-        if(!this.raiz){
-            this.raiz=nuevoNodo
-        }else{
-            this.insertarNodo(this.raiz,nuevoNodo)
-        }
+  insertarValorEnArbol=(val)=>{
+     const nuevoNodo = new Nodo(val)
+     if(!this.raiz){
+        this.raiz=nuevoNodo
+     }else{
+        this._insertarNodo(this.raiz,nuevoNodo)
      }
+  }
+  _insertarNodo=(nodoActual,nuevoNodo)=>{
+    if(nuevoNodo.value<nodoActual.value){
+       if(!nodoActual.izquierdo){
+        nodoActual.izquierdo=nuevoNodo
+       }else{
+        this._insertarNodo( nodoActual.izquierdo,nuevoNodo)
+       }
+    }else{
+        if(!nodoActual.derecho){
+            nodoActual.derecho=nuevoNodo
+        }else{
+            this._insertarNodo(nodoActual.derecho,nuevoNodo)
+        }
 
-     insertarNodo=(nodo, nuevonodo)=>{
-        if(nuevonodo.valor===nodo.valor){
-            console.log("Este valor ya existe")
-         return 
-        }   
-        if(nuevonodo.valor<nodo.valor){
-            if(!nodo.izquerdo){
-                nodo.izquerdo=nuevonodo
-            }else{
-                this.insertarNodo(nodo.izquerdo,nuevonodo)
-            }
-        }else{
-            if(!nodo.derecho){
-                nodo.derecho=nuevonodo;
-            }else{
-                this.insertarNodo(nodo.derecho,nuevonodo)
-            }
-        }
-        
-     }
+    }
+  }
 }
-     
 
 
 const miArbol=new ArbolBinario()
-miArbol.insertarValorenArbol(15)
-miArbol.insertarValorenArbol(16)
-miArbol.insertarValorenArbol(17)
-miArbol.insertarValorenArbol(18)
-miArbol.insertarValorenArbol(14)
-miArbol.insertarValorenArbol(13)
-miArbol.insertarValorenArbol(11)
-miArbol.insertarValorenArbol(12)
-console.log(miArbol)
+miArbol.insertarValorEnArbol(15)
+miArbol.insertarValorEnArbol(16)
+miArbol.insertarValorEnArbol(17)
+miArbol.insertarValorEnArbol(18)
+miArbol.insertarValorEnArbol(14)
+miArbol.insertarValorEnArbol(13)
+miArbol.insertarValorEnArbol(11)
+miArbol.insertarValorEnArbol(12)
+console.log(JSON.stringify(miArbol, null, 2));
